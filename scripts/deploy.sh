c1818c07f76e5d7e48d800182e9162e3a316dad1
toboid <toboid@users.noreply.github.com>
2015-10-05 16:42:57 +0100

    Generalise deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,25 @@
 
 set -e
 
-export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-export CLOUDSDK_PYTHON_SITEPACKAGES=1
+usage="Usage: './deploy.sh image-name selector' e.g. './deploy.sh my-img-name app=myApp'"
+
+if [[ $# -ne 2 ]]; then
+    echo "Incorrect number of arguments, 2 required";
+    echo $usage;
+    exit 1;
+fi
+
+IMAGE=$1;
+SELECTOR=$2;
 
 VERSION_ID=${CIRCLE_SHA1:0:7}
 REMOTE_REPOSITORY=gcr.io/${CLOUDSDK_CORE_PROJECT}
 
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+
 # Build image
-docker build -t ${REMOTE_REPOSITORY}/nginx-api-proxy:${VERSION_ID} .
+docker build -t ${REMOTE_REPOSITORY}/${IMAGE}:${VERSION_ID} .
 
 # Authenticate gcloud SDK
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
@@ -20,8 +31,8 @@ ssh-keygen -f ~/.ssh/google_compute_engine -N ""
 ~/google-cloud-sdk/bin/gcloud container clusters get-credentials $GCLOUD_CLUSTER
 
 # Push image to gcloud
-~/google-cloud-sdk/bin/gcloud docker push ${REMOTE_REPOSITORY}/nginx-api-proxy:${VERSION_ID} > /dev/null
+~/google-cloud-sdk/bin/gcloud docker push ${REMOTE_REPOSITORY}/${IMAGE}:${VERSION_ID} > /dev/null
 
 # Rolling update
-OLD_RC=$(~/google-cloud-sdk/bin/kubectl get rc -l app=nginx | cut -f1 -d " " | tail -1)
-~/google-cloud-sdk/bin/kubectl rolling-update ${OLD_RC} nginx-api-proxy-${VERSION_ID} --image=${REMOTE_REPOSITORY}/nginx-api-proxy:${VERSION_ID}
+OLD_RC=$(~/google-cloud-sdk/bin/kubectl get rc -l ${SELECTOR} | cut -f1 -d " " | tail -1)
+~/google-cloud-sdk/bin/kubectl rolling-update ${OLD_RC} ${IMAGE}-${VERSION_ID} --image=${REMOTE_REPOSITORY}/${IMAGE}:${VERSION_ID}
