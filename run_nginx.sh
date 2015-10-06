d9a8f27f5ca543841d879c3a45d66b99b03b6011
toboid <toboid@users.noreply.github.com>
2015-10-06 10:58:41 +0100

    Improve comments

--- run_nginx.sh
+++ run_nginx.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# Write environment variables into the nginx config (they are not supported natively)
 (echo "upstream search_svc_url { server $SEARCH_API_SVC_SERVICE_HOST:$SEARCH_API_SVC_SERVICE_PORT; }" && \
   cat /etc/nginx/conf.d/big-wednesday.conf) > big-wednesday.conf.updated
 mv big-wednesday.conf.updated /etc/nginx/conf.d/big-wednesday.conf
