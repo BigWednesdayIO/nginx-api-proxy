12081ee2ecfe506dec84f40ba459ed29a8544397
Chris Impey <chris@bigwednesday.io>
2015-11-06 11:40:48 +0000

    add checkouts route to proxy

--- run_nginx.sh
+++ run_nginx.sh
@@ -4,11 +4,10 @@ set -e
 
 # Write environment variables into the nginx config (they are not supported natively)
 (echo "upstream search_svc_url { server $SEARCH_API_SVC_SERVICE_HOST:$SEARCH_API_SVC_SERVICE_PORT; }" && \
-  cat /etc/nginx/conf.d/big-wednesday.conf) > big-wednesday.conf.updated
-mv big-wednesday.conf.updated /etc/nginx/conf.d/big-wednesday.conf
+echo "upstream checkout_svc_url { server $CHECKOUT_API_SVC_SERVICE_HOST:$CHECKOUT_API_SVC_SERVICE_PORT; }" && \
+  cat /etc/nginx/conf.d/big-wednesday.conf.template) > /etc/nginx/conf.d/big-wednesday.conf
 
 echo "Using config"
 cat /etc/nginx/conf.d/big-wednesday.conf
 
 nginx -g "daemon off;";
-
